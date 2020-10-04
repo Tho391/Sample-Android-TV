8a8e52225a075768bf6d9c3b8a47d30423c15345
Thomas <quangthonguyen391@gmail.com>
2020-10-04 15:51:19 +0700

    add fused location provider client

--- app/src/main/java/com/example/mapandroidtv/MainActivity.kt
+++ app/src/main/java/com/example/mapandroidtv/MainActivity.kt
@@ -3,37 +3,25 @@ package com.example.mapandroidtv
 import android.Manifest
 import android.annotation.SuppressLint
 import android.content.Context
-import android.location.Location
-import android.location.LocationManager
+import android.location.*
+import android.location.LocationListener
 import android.os.Bundle
+import android.os.Looper
 import android.util.Log
-import android.widget.Toast
 import androidx.fragment.app.FragmentActivity
 import com.example.mapandroidtv.databinding.ActivityMainBinding
-import com.google.android.gms.location.FusedLocationProviderClient
-import com.google.android.gms.location.LocationRequest
-import com.google.android.gms.location.LocationServices
+import com.google.android.gms.location.*
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.CameraPosition
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
-import com.karumi.dexter.Dexter
-import com.karumi.dexter.MultiplePermissionsReport
-import com.karumi.dexter.PermissionToken
-import com.karumi.dexter.listener.PermissionRequest
-import com.karumi.dexter.listener.multi.MultiplePermissionsListener
 
-
-/**
- * Loads [MainFragment].
- */
 class MainActivity : FragmentActivity() {
 
     private lateinit var binding: ActivityMainBinding
     private lateinit var googleMap: GoogleMap
-    private lateinit var fusedLocationClient: FusedLocationProviderClient
 
     private val permissions = listOf(
         Manifest.permission.ACCESS_COARSE_LOCATION
@@ -47,16 +35,101 @@ class MainActivity : FragmentActivity() {
         setUpMap()
 
         binding.button.setOnClickListener {
-            requestLocation()
-
+            Log.i("Main", "button click")
+//            requestLocationViaLocationManager()
+            requestLocationViaFuseLocationClient()
         }
     }
 
-    private fun requestLocation() {
-        //fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
 
-        val location = getLocationByProvider(LocationManager.NETWORK_PROVIDER)
-        Log.i("Main", "Location: ${location?.latitude}")
+    @SuppressLint("MissingPermission")
+    private fun requestLocationViaFuseLocationClient() {
+        val fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this)
+
+        val locationRequest = LocationRequest.create().apply {
+            priority = LocationRequest.PRIORITY_HIGH_ACCURACY
+            interval = 10 * 1000 //10s
+            fastestInterval = 5 * 1000
+        }
+
+//        fusedLocationProviderClient.lastLocation
+//            .addOnSuccessListener {
+//                Log.i("Main", "addOnSuccessListener: ${it?.latitude}")
+//            }.addOnFailureListener {
+//                Log.e("Main", "addOnFailureListener: ${it.message}")
+//            }
+
+
+        fusedLocationProviderClient.requestLocationUpdates(
+            locationRequest,
+            object : LocationCallback() {
+                override fun onLocationResult(p0: LocationResult?) {
+                    Log.i("Main", "onLocationResult: ${p0?.locations?.size}")
+                }
+            },
+            Looper.getMainLooper()
+        )
+    }
+
+    @SuppressLint("MissingPermission")
+    private fun requestLocationViaLocationManager() {
+
+        // Request a static location from the location manager
+
+        val locationManager = this.getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        val criteria = Criteria()
+        criteria.accuracy = Criteria.ACCURACY_FINE
+        val provider = locationManager.getBestProvider(criteria, false)
+        Log.i("Main", "provider: $provider")
+//        provider?.let {
+//            val location = locationManager.getLastKnownLocation(provider)
+//            Log.i("Main", "location: ${location?.latitude}")
+//        }
+
+        provider?.let {
+            locationManager.requestLocationUpdates(
+                provider,
+                100,
+                10F,
+                object : LocationListener {
+                    override fun onLocationChanged(location: Location?) {
+                        Log.i("Main", "onLocationChanged")
+                    }
+
+                    override fun onStatusChanged(provider: String?, status: Int, extras: Bundle?) {
+                        Log.i("Main", "onStatusChanged")
+                    }
+
+                    override fun onProviderEnabled(provider: String?) {
+                        Log.i("Main", "onProviderEnabled")
+
+                    }
+
+                    override fun onProviderDisabled(provider: String?) {
+                        Log.i("Main", "onProviderDisabled")
+
+                    }
+
+                })
+        }
+
+//        fusedLocationClient.requestLocationUpdates(
+//            getLocationRequest(),
+//            object : LocationCallback() {
+//                override fun onLocationAvailability(p0: LocationAvailability?) {
+//                    super.onLocationAvailability(p0)
+//                }
+//
+//                override fun onLocationResult(p0: LocationResult?) {
+//                    super.onLocationResult(p0)
+//                }
+//            },
+//            Looper.getMainLooper()
+//        )
+
+
+        //val location = getLocationByProvider(LocationManager.NETWORK_PROVIDER)
+        //Log.i("Main", "Location: ${location?.latitude}")
 
 //        Dexter.withContext(this)
 //            .withPermissions(permissions)
@@ -95,14 +168,6 @@ class MainActivity : FragmentActivity() {
         )
     }
 
-    private fun getLocationRequest(): LocationRequest {
-        return LocationRequest.create().apply {
-            interval = 10000
-            fastestInterval = 5000
-            priority = LocationRequest.PRIORITY_HIGH_ACCURACY
-        }
-    }
-
     private fun setUpMap() {
         val mapFragment = supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync {
@@ -114,23 +179,5 @@ class MainActivity : FragmentActivity() {
                     .title("Marker")
             )
         }
-    }
-
-    @SuppressLint("MissingPermission")
-    private fun getLocationByProvider(provider: String): Location? {
-        var location: Location? = null
-        val locationManager: LocationManager = applicationContext
-            .getSystemService(Context.LOCATION_SERVICE) as LocationManager
-        if (!locationManager.isProviderEnabled(provider)) {
-            return null
-        }
-        try {
-            if (locationManager.isProviderEnabled(provider)) {
-                location = locationManager.getLastKnownLocation(provider)
-            }
-        } catch (e: IllegalArgumentException) {
-            Log.d("Main", "Cannot access Provider $provider")
-        }
-        return location
     }
 }
